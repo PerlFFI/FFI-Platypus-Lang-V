b06708141bbcabb114cbd95cb8bf450688e011e8
Graham Ollis <plicease@cpan.org>
2024-12-18 06:10:05 -0700

    Initial structure

---
+++ lib/FFI/Platypus/Lang/V.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package FFI::Platypus::Lang::V {
+
+  # ABSTRACT: Documentation and tools for using Platypus with the V programming language
+}
+
+1;
+
